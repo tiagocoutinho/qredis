2f247f4f9e576ee60520bb9cd1b1dd208809efd6
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2017-03-25 20:08:06 +0100

    Clean up unused test mess

--- setup.py
+++ setup.py
@@ -13,15 +13,6 @@ requirements = [
     'PyQt4',
 ]
 
-test_requirements = [
-    'pytest',
-    'pytest-cov',
-    'pytest-faulthandler',
-    'pytest-mock',
-    'pytest-qt',
-    'pytest-xvfb',
-]
-
 setup(
     name='qredis',
     version=qredis.__version__,
@@ -29,8 +20,7 @@ setup(
     author="Tiago Coutinho",
     author_email='coutinhotiago@gmail.com',
     url='https://github.com/tiagocoutinho/qredis',
-    packages=['qredis', 'qredis.images', 'qredis.ui',
-              'qredis.tests'],
+    packages=['qredis', 'qredis.images', 'qredis.ui'],
     package_data={'qredis.images': ['*.png'],
                   'qredis.ui': ['*.ui'],},
     entry_points={
@@ -42,11 +32,11 @@ setup(
     zip_safe=False,
     keywords='redis',
     classifiers=[
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
 )
